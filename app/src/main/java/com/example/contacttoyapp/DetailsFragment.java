91718f14298536c75a4667ff8a25d6203a202999
Kelli Davis <keldavis5@yahoo.com>
2019-02-28 13:04:58 -0500

    refactor: Modify projection to retrieve the columns that are specific to the data type - modify the projection to use the column name constants defined in the ContactsContract.CommonDataKinds subclass corresponding to the data type.

--- app/src/main/java/com/example/contacttoyapp/DetailsFragment.java
+++ app/src/main/java/com/example/contacttoyapp/DetailsFragment.java
@@ -13,23 +13,10 @@ import android.support.v4.content.Loader;
 public class DetailsFragment extends Fragment implements
         LoaderManager.LoaderCallbacks<Cursor>{
     private static final String[] PROJECTION = {
-            ContactsContract.Data._ID,
-            ContactsContract.Data.MIMETYPE,
-            ContactsContract.Data.DATA1,
-            ContactsContract.Data.DATA2,
-            ContactsContract.Data.DATA3,
-            ContactsContract.Data.DATA4,
-            ContactsContract.Data.DATA5,
-            ContactsContract.Data.DATA6,
-            ContactsContract.Data.DATA7,
-            ContactsContract.Data.DATA8,
-            ContactsContract.Data.DATA9,
-            ContactsContract.Data.DATA10,
-            ContactsContract.Data.DATA11,
-            ContactsContract.Data.DATA12,
-            ContactsContract.Data.DATA13,
-            ContactsContract.Data.DATA14,
-            ContactsContract.Data.DATA15
+            ContactsContract.CommonDataKinds.Email._ID,
+            ContactsContract.CommonDataKinds.Email.ADDRESS,
+            ContactsContract.CommonDataKinds.Email.TYPE,
+            ContactsContract.CommonDataKinds.Email.LABEL
     };
 
     // Defines the selection clause
