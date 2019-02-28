ac16116a8877d369458661160e9785faa00e8b38
Kelli Davis <keldavis5@yahoo.com>
2019-02-28 11:49:57 -0500

    feat: Set up a projection
    Depending on the data type a row contains, it may use only a few columns or many. In addition, the data is in different columns depending on the data type. To ensure you get all the possible columns for all possible data types, you need to add all the column names to your projection. Always retrieve Data._ID if you're binding the result Cursor to a ListView; otherwise, the binding won't work. Also retrieve Data.MIMETYPE so you can identify the data type of each row you retrieve.

---
+++ app/src/main/java/com/example/contacttoyapp/DetailsFragment.java
@@ -0,0 +1,25 @@
+package com.example.contacttoyapp;
+
+import android.provider.ContactsContract;
+
+public class DetailsFragment {
+    private static final String[] PROJECTION = {
+            ContactsContract.Data._ID,
+            ContactsContract.Data.MIMETYPE,
+            ContactsContract.Data.DATA1,
+            ContactsContract.Data.DATA2,
+            ContactsContract.Data.DATA3,
+            ContactsContract.Data.DATA4,
+            ContactsContract.Data.DATA5,
+            ContactsContract.Data.DATA6,
+            ContactsContract.Data.DATA7,
+            ContactsContract.Data.DATA8,
+            ContactsContract.Data.DATA9,
+            ContactsContract.Data.DATA10,
+            ContactsContract.Data.DATA11,
+            ContactsContract.Data.DATA12,
+            ContactsContract.Data.DATA13,
+            ContactsContract.Data.DATA14,
+            ContactsContract.Data.DATA15
+    };
+}
