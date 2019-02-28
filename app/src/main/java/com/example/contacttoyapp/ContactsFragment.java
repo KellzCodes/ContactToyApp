dda0b61fb718c50a76f8e34e8e7a32da5f7133a5
Kelli Davis <keldavis5@yahoo.com>
2019-02-27 23:09:15 -0500

    feat: Define global variables

--- app/src/main/java/com/example/contacttoyapp/ContactsFragment.java
+++ app/src/main/java/com/example/contacttoyapp/ContactsFragment.java
@@ -1,6 +1,9 @@
 package com.example.contacttoyapp;
 
+import android.annotation.SuppressLint;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -9,12 +12,46 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class ContactsFragment extends Fragment implements
         LoaderManager.LoaderCallbacks<Cursor>,
         AdapterView.OnItemClickListener{
+    /*
+    * Defines an array that contains column names to move from the Cursor
+    * to the ListView
+    * */
+    @SuppressLint("InlinedApi")
+    private final static String[] FROM_COLUMNS = {
+            Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+                    ContactsContract.Contacts.DISPLAY_NAME
+    };
+
+    /*
+    * Defines an array that contains resource ids for the layout views
+    * that get the Cursor column contents. THe id is pre-defined in
+    * the Android framework, so it is prefaced with "android.R.id"*/
+    private final static int[] TO_IDS = {
+            android.R.id.text1
+    };
+
+    // Define global mutable variables
+    // Define a ListView object
+    ListView contactsList;
+    // Define variables for the contact the user selects
+    // The contact's _ID value
+    long contactId;
+    // The contact's LOOKUP_KEY
+    String contactKey;
+    // A content URI for the selected contact
+    Uri contactUri;
+    // An adapter that binds the result Cursor to the ListView
+    private SimpleCursorAdapter cursorAdapter;
 
     @NonNull
     @Override
